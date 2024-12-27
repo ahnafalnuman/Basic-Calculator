17e96a89013f169f87df23454bccbe94265a878e
Ahnaf Alnuman <omariahnaf@gmail.com>
2024-12-27 19:13:21 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Calculator
+    {
+        // Properties 
+        public double X { get; }
+        public double Y { get; }
+
+        // Constractor
+        public Calculator(double x, double y)
+        {
+            this.X = x;
+            this.Y = y; 
+        }
+        // ADD
+        public double Add()
+        {
+            return X + Y;
+        }
+
+        // SUB
+        public double Subtract()
+        {
+            return X - Y;
+        }
+
+        // MULT
+        public double Multiply()
+        {
+            return X * Y;
+        }
+
+        // DIV
+        public string Divide()
+        {
+            try
+            {
+
+                if (Y == 0)
+                {
+                    return "Cannot divide by zero";
+                }
+                else
+                {
+                    return (X / Y).ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"An error occurred: {ex.Message}";
+            }
+        }
+
+        //Method display all result 
+        public string GetResults()
+        {
+            return $"Sum ({X} + {Y}): {Add()}\n" +
+                   $"Difference ( {X} - {Y}): {Subtract()}\n" +
+                   $"Product ({X} * {Y}): {Multiply()}\n" +
+                   $"Quotient ({X} / {Y}): {Divide()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 0; 
+            double y = 0; 
+            bool validInput = false; // Declare validInput
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter the value for x: ");
+                    x = double.Parse(Console.ReadLine());
+
+                    Console.Write("Enter the value for y: ");
+                    y = double.Parse(Console.ReadLine());
+
+                    validInput = true; // Input is valid  THEN .. exit the loop
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter numeric values.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+
+            } while (!validInput);
+
+
+
+            Calculator calc = new Calculator(x, y);
+
+            Console.WriteLine(calc.GetResults());
+
+            
+        }
+    }
+}
